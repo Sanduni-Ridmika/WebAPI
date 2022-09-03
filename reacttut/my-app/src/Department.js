4714b24c460f65e495276b2085d16ec2d4634d7d
28sanduni <99sanduni99@gmail.com>
2022-09-03 18:51:12 +0530

    Store API and department data

--- reacttut/my-app/src/Department.js
+++ reacttut/my-app/src/Department.js
@@ -2,6 +2,27 @@ import React,{Component} from 'react';
 
 export class Department extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={deps:[]}; 
+    }
+    
+    refreshList(){
+        fetch(process.env.REACT_APP_API+'department')
+        .then(response=>response.json())
+        .then(data=>{
+            this.setState({deps:data});
+        });
+    }
+
+    ComponentDidMount(){
+        this.refreshList();
+    }
+
+    componentDidUpdate(){
+        this.refreshList();
+    }
+
     render(){
         return(
         <div className="mt-5 d-flex justify-content-left">
